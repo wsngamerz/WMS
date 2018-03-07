956f84af341d72694a2e1ff816467f9ac3a2103e
William Neild <wneild5@gmail.com>
2018-03-07 18:24:04 +0000

    added test user data

--- wms/routes.py
+++ wms/routes.py
@@ -83,7 +83,12 @@ class Routes:
         else:
             # pageData["user"]["data"] = db.Get.user(session["loggedIn"], pageData)
             # TODO: DB Get class
+            # DEV: Using Temporary Hard-Coded Data to fill Templates
             pageData["user"]["loggedIn"] = True
+            pageData["user"]["data"] = {}
+            pageData["user"]["data"]["firstName"] = "TestFirstName"
+            pageData["user"]["data"]["lastName"] = "TestLastName"
+            pageData["user"]["data"]["username"] = "TestUsername"
         pageData["request"] = {}
         pageData["request"]["rootURL"] = str(request.url_root)
         return pageData
