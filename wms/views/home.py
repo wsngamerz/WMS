ef733a90e42e403847c464072e3915d13b36f8af
William Neild <wneild5@gmail.com>
2018-03-11 12:11:02 +0000

    Fixed pageName and added register POST method

--- wms/views/home.py
+++ wms/views/home.py
@@ -16,12 +16,12 @@ class homeBlueprint:
         @home.route("/")
         def homePage():
             pageConfig = pageData(self.configData)
-            return render_template("home/home.html", config=pageConfig)
+            return render_template("home/home.html", pageName="Home", config=pageConfig)
 
         @home.route("/login/", methods=["GET"])
         def loginPage():
             pageConfig = pageData(self.configData)
-            return render_template("home/login.html", config=pageConfig)
+            return render_template("home/login.html", pageName="Login", config=pageConfig)
 
         @home.route("/login/", methods=["POST"])
         def loginAction():
@@ -34,12 +34,24 @@ class homeBlueprint:
         @home.route("/register/")
         def registerPage():
             pageConfig = pageData(self.configData)
-            return render_template("home/register.html", config=pageConfig)
+            return render_template("home/register.html", pageName="Register", config=pageConfig)
+
+        @home.route("/register/", methods=["POST"])
+        def registerAction():
+            pageConfig = pageData(self.configData)
+            fname = str(request.form["firstName"])
+            lname = str(request.form["lastName"])
+            username = str(request.form["username"])
+            password = str(request.form["password"])
+            passwordConfirm = str(request.form["passwordConfirm"])
+            email = str(request.form["email"])
+            session["loggedIn"] = True
+            return redirect(str(pageConfig["request"]["rootURL"]), code=302)
 
         @home.route("/logout/", methods=['GET'])
         def logoutPage():
             pageConfig = pageData(self.configData)
-            return render_template("home/logout.html", config=pageConfig)
+            return render_template("home/logout.html", pageName="Logout", config=pageConfig)
 
         @home.route("/logout/", methods=['POST'])
         def logoutAction():
