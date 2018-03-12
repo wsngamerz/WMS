01f2a49dd27e8eabf516b20512939d69d2f584f2
William Neild <wneild5@gmail.com>
2018-03-12 20:07:16 +0000

    Added Permissions and Settings Database and Updated User DB

--- wms/db.py
+++ wms/db.py
@@ -7,10 +7,30 @@ class DB:
     def __init__(self, config):
         logging.info("Initialising Databases")
 
-# class Main(db.Model):
-#     __bind_key__ = "main"
+class ServerSettings(db.Model):
+    __bind_key__ = "main"
+    __tablename__ = "serverSettings"
+    id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(64))
+    category = db.Column("category", db.String(64))
+    value = db.Column("value", db.Text)
+    description = db.Column("description", db.Text)
 
+# Tables for the Users Database
 class User(db.Model):
     __bind_key__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
+    __tablename__ = "user"
+    id = db.Column("userID", db.Integer, primary_key=True)
+    username = db.Column("username", db.String(255), unique=True)
+    firstName = db.Column("firstName", db.String(255))
+    lastName = db.Column("lastName", db.String(255))
+    fullName = db.column_property(firstName + " " + lastName)
+    email = db.Column("email", db.String(255))
+    permission = db.relationship("Permission", backref="user", lazy=True)
+
+class Permission(db.Model):
+    __bind_key__ = "users"
+    __tablename__ = "permissions"
+    id = db.Column("permissionID", db.Integer, primary_key=True)
+    level = db.Column("level", db.Integer)
+    levelName = db.Column("levelName", db.String(64))
